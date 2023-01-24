7c7bd8272672dc9af64f10cebc09d764c5b77d3d
matteo-cristino <cristino.matteo97@gmail.com>
2023-01-24 18:36:43 +0100

    fix handling of promises and prepare for tests

--- bindings/javascript/src/index.ts
+++ bindings/javascript/src/index.ts
@@ -3,33 +3,29 @@ import { join } from 'path';
 import axios from 'axios';
 import { zencode_exec } from 'zenroom';
 
+const readFromFile = (
+  pathToFile: string
+) :string => {
+  return readFileSync(join(__dirname, pathToFile), 'utf-8');
+}
 
 export const createKeyring = async (
     description: string
 ) :Promise<string> => {
-  const contract = readFileSync(join(__dirname, 'create-keyring.zen'), 'utf-8');
-  const data = `{"controller": "${description}"}`; //maybe description.replace(/ /g,"_")
-  let result: string;
-  try {
-    ({result} = await zencode_exec(contract, {data, keys : "{}"}));
-  } catch(e) {
-    console.log(e);
-  }
+  const contract = readFromFile('client/v1/create-keyring.zen');
+  const data = JSON.stringify({ controller: description}); //maybe description.replace(/ /g,"_")
+  const {result} = await zencode_exec(contract, {data, keys : "{}"});
   return result;
 }
+
 const preparePks = async (
   requestKeyring: string,
   requestDomain: string,
   signerDomain: string
 ) :Promise<string> => {
-  const contractPks = readFileSync(join(__dirname, 'create-identity-pubkeys.zen'), 'utf-8');
-  const data = readFileSync(join(__dirname, 'did-settings.json'), 'utf-8');
-  let result: string;
-  try {
-    ({result} = await zencode_exec(contractPks, {data, keys : requestKeyring}));
-  } catch(e) {
-    console.log(e);
-  }
+  const contractPks = readFromFile('client/v1/create-identity-pubkeys.zen');
+  const data = readFromFile('client/v1/did-settings.json');
+  let {result} = await zencode_exec(contractPks, {data, keys : requestKeyring});
   result = JSON.parse(result);
   result["did_spec"] = requestDomain;
   result["signer_did_spec"] = signerDomain;
@@ -43,14 +39,9 @@ export const createRequest = async (
   signerKeyring: string,
   signerDomain: string
 ) :Promise<string> => {
-  const contractRequest = readFileSync(join(__dirname, 'pubkeys-request-signed.zen'), 'utf-8');
+  const contractRequest = readFromFile('client/v1/pubkeys-request-signed.zen');
   const data = await preparePks(requestKeyring, requestDomain, signerDomain);
-  let result: string;
-  try {
-    ({result} = await zencode_exec(contractRequest, {data, keys : signerKeyring}));
-  } catch(e) {
-    console.log(e);
-  }
+  const {result} = await zencode_exec(contractRequest, {data, keys : signerKeyring});
   return result;
 }
 
@@ -60,14 +51,9 @@ export const deactivateRequest = async (
   signerKeyring: string,
   signerDomain: string
 ) :Promise<string> => {
-  const contractRequest = readFileSync(join(__dirname, 'pubkeys-deactivate.zen'), 'utf-8');
+  const contractRequest = readFromFile('client/v1/pubkeys-deactivate.zen');
   const data = await preparePks(requestKeyring, requestDomain, signerDomain);
-  let result: string;
-  try {
-    ({result} = await zencode_exec(contractRequest, {data, keys : signerKeyring}));
-  } catch(e) {
-    console.log(e);
-  }
+  const {result} = await zencode_exec(contractRequest, {data, keys : signerKeyring});
   return result;
 }
 
@@ -76,10 +62,8 @@ export const sendRequest = async (
   request: string
 ) :Promise<string> => {
   const res = await axios.post(
-    `https://did.dyne.org:443/${endpoint}`,
+    `https://did.dyne.org/${endpoint}`,
     { data: JSON.parse(request), keys: "{}"}
-    )
-    .then((response) => response.data)
-    .catch((err) => console.log(err));
-  return res;
+  )
+  return res.data;
 }
